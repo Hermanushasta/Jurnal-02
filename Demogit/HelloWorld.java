2a2011128afa962624aceaab495270eb334c2b94
Hermanushasta <hasta.wicaksana@gmail.com>
2021-09-30 12:43:01 +0700

    HelloWorld.java

---
+++ Demogit/HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+	public static void main(String args[]){
+		System.out.println("Nama : Hermanus Hasta Wicaksana");
+		System.out.println("NIM  : 6706210043");
+		System.out.println("Kelas: D3IF-45-05");
+		System.out.println("IPK  : 4.0");
+	}
+} 
